cb3134cd6ede6a4c4fb4aad1db4205b2064d93cb
Christopher Montiel <christopher.montiel.m@gmail.com>
2022-06-26 11:07:11 -0400

    [ADD] Se incorpora CORS para la seguridad de la conexión http

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const config = require('./config/config');
 const routerApi = require('./routes');
 
@@ -6,6 +7,19 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+
+const whiteList = ['http://localhost:3000'];
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  },
+};
+app.use(cors(options));
+
 routerApi(app);
 
 app.listen(port, () => {
